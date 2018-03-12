134d0bd2984f6bd6a3c7b864c56217171add61cd
Kohki Miki <koki-miki@cookpad.com>
2018-03-12 11:11:39 +0900

    Add test case for recent Carthage

--- test/anticuado/ios/carthage_test.rb
+++ test/anticuado/ios/carthage_test.rb
@@ -11,6 +11,13 @@ The following dependencies are outdated:
 Result "2.0.0" -> "2.1.3"
       OUTDATED
 
+
+      OUTDATED_HAVE_UPDATE_WITH_LATEST =<<-OUTDATED
+*** Fetching Puree-Swift
+The following dependencies are outdated:
+Puree-Swift "3.0.0" -> "3.0.0" (Latest: "3.1.1")
+      OUTDATED
+
       OUTDATED_NO_UPDATE =<<-OUTDATED
 *** Fetching Result
 *** Fetching Himotoki
@@ -22,6 +29,14 @@ All dependencies are up to date.
         result = Anticuado::IOS::Carthage.format OUTDATED_HAVE_UPDATE
 
         expected_0 = { library_name: "Result", current_version: "2.0.0", available_version: "2.1.3", latest_version: "2.1.3" }
+
+        assert_equal expected_0, result[0]
+      end
+
+      def test_with_format_have_update_with_latest
+        result = Anticuado::IOS::Carthage.format OUTDATED_HAVE_UPDATE_WITH_LATEST
+
+        expected_0 = { library_name: "Puree-Swift", current_version: "3.0.0", available_version: "3.0.0", latest_version: "3.1.1" }
 
         assert_equal expected_0, result[0]
       end
